6efc17fdc198cd7430a9882b227e77e05dcde64c
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-08-09 19:27:51 -0400

    RPS refactor 1

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -2,22 +2,16 @@ print("Rock...")
 print("Paper...")
 print("Scissors...")
 
-player1 = input("Player 1, make your move: ")
-player2 = input("Player 2, make your move: ")
-
-if player1 == "rock" and player2 == "scissors":
-	print("player1 wins!")
-elif player1 == "rock" and player2 == "paper":
-	print("player2 wins!")
-elif player1 == "paper" and player2 == "rock":
-	print("player1 wins!")
-elif player1 == "paper" and player2 == "scissors":
-	print("player2 wins!")
-elif player1 == "scissors" and player2 == "rock":
-	print("player2 wins!")
-elif player1 == "scissors" and player2 == "paper":
-	print("player1 wins!")
-elif player1 == player2:
-	print("It's a tie!")
+p1 = input("Player 1: rock, paper, or scissors ")
+p2 = input("Player 2: rock, paper, or scissors ")
+ 
+if p1 == p2:
+    print("Draw")
+elif p1 == "rock" and p2 == "scissors":
+    print("p1 wins")
+elif p1 == "paper" and p2 == "rock":
+    print("p1 wins")
+elif p1 == "scissors" and p2 == "paper":
+    print("p1 wins")
 else:
-	print("Something went wrong")
+    print("p2 wins")
